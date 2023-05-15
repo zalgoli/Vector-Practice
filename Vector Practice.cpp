5130c4f50e4f152f6a28debf29f83ca9189e8183
zalgoli <galzoltan.jr@gmail.com>
2023-05-15 22:46:25 +0200

    Added 3D vectors

--- Vector Practice.cpp
+++ Vector Practice.cpp
@@ -51,5 +51,79 @@ int main() {
 		}
 	}
 
+	// 3D-VECTOR PRACTICE
+
+	std::cout << "\nProvide the number of rows, columns and layers for the 3D matrix:\n";
+	int rows2, columns2, layers;
+	std::cin >> rows2;
+	std::cin >> columns2;
+	std::cin >> layers;
+
+	std::cout << "You can choose to input custom elements (which will be sorted) or to input the first N numbers (input 1 or 2): \n";
+	int choice;
+	std::cin >> choice;
+	switch (choice) {
+	case 1: {
+		std::vector<std::vector<std::vector<int>>> vec3D(layers, std::vector<std::vector<int>>(rows, std::vector<int>(columns)));
+
+		// Taking input
+		for (int i = 0; i < vec3D.size(); i++) {
+			for (int j = 0; j < vec3D[i].size(); j++) {
+				for (int k = 0; k < vec3D[i][j].size(); k++) {
+					std::cin >> vec3D[i][j][k];
+				}
+			}
+		}
+
+		// Sorting
+		for (int i = 0; i < vec3D.size(); i++) {
+			std::sort(vec3D.begin(), vec3D.end());
+			for (int j = 0; j < vec3D[i].size(); j++) {
+				std::sort(vec3D[i].begin(), vec3D[i].end());
+				for (int k = 0; k < vec3D[i][j].size(); k++) {
+					std::sort(vec3D[i][j].begin(), vec3D[i][j].end());
+				}
+			}
+		}
+
+		// Outputting
+		for (int i = 0; i < vec3D.size(); i++) {
+			std::cout << "\n\n\n";
+			for (int j = 0; j < vec3D[i].size(); j++) {
+				std::cout << "\n";
+				for (int k = 0; k < vec3D[i][j].size(); k++) {
+					std::cout << vec3D[i][j][k] << " ";
+				}
+			}
+		}
+		break;
+	}
+	case 2: {
+		std::vector<std::vector<std::vector<int>>> vec3D(layers, std::vector<std::vector<int>>(rows2, std::vector<int>(columns2)));
+
+		// Populating
+		int counter = 0;
+		for (int i = 0; i < vec3D.size(); i++) {
+			for (int j = 0; j < vec3D[i].size(); j++) {
+				for (int k = 0; k < vec3D[i][j].size(); k++) {
+					vec3D[i][j][k] = counter++;
+				}
+			}
+		}
+
+		// Outputting
+		for (int i = 0; i < vec3D.size(); i++) {
+			std::cout << "\n\n\n";
+			for (int j = 0; j < vec3D[i].size(); j++) {
+				std::cout << "\n";
+				for (int k = 0; k < vec3D[i][j].size(); k++) {
+					std::cout << vec3D[i][j][k] << " ";
+				}
+			}
+		}
+		break;
+	}
+	}
+
 	return 0;
 }
